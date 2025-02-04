b43cee9c57ded075ee1c5918b1262655ccc687aa
Pedro Henrique Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2025-02-04 09:44:17 -0300

    ajustes no projeto

--- src/js/script.js
+++ src/js/script.js
@@ -4,19 +4,42 @@ const apiUrl =
 
 const campoInput = document.querySelector(".search input");
 const btnBuscar = document.querySelector(".search button");
+const climaIcon = document.querySelector(".clima-icon");
 
 async function verificarClima(cidade) {
   const response = await fetch(apiUrl + cidade + `&appid=${apiKey}`);
-  let data = await response.json();
 
-  console.log(data);
+  if (response.status !== 200) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".clima").style.display = "none";
+  } else {
+    document.querySelector(".error").style.display = "none";
+    let data = await response.json();
 
-  document.querySelector(".cidade").innerHTML = data.name;
+    console.log(data);
 
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".cidade").innerHTML = data.name;
 
-  document.querySelector(".humidade").innerHTML = data.main.humidity + "%";
-  document.querySelector(".vento").innerHTML = data.wind.speed + " Km/h";
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+
+    document.querySelector(".humidade").innerHTML = data.main.humidity + "%";
+    document.querySelector(".vento").innerHTML = data.wind.speed + " Km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      climaIcon.src = "./public/imgs/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      climaIcon.src = "./public/imgs/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      climaIcon.src = "./public/imgs/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      climaIcon.src = "./public/imgs/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      climaIcon.src = "./public/imgs/mist.png";
+    }
+
+    document.querySelector(".clima").style.display = "block";
+  }
 }
 
 btnBuscar.addEventListener("click", () => {
